aa3400eb448ee59fb0ac7f68ec8a36e30e733454
lee <mrleeapple@163.com>
2019-06-20 18:25:29 +0800

    test2

--- chapter-3-spring-boot-web/src/main/java/demo/springboot/WebApplication.java
+++ chapter-3-spring-boot-web/src/main/java/demo/springboot/WebApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class WebApplication {
-    //testDD
+    //
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
